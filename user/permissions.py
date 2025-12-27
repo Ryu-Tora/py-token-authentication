3496eaaf814c13eabff550f426fe15db2e3f5322
Adam Halka <galkaadam4@gmail.com>
2025-12-27 19:43:51 +0100

    Solution

--- user/permissions.py
+++ user/permissions.py
@@ -1,14 +1,13 @@
-from rest_framework import permissions
-from rest_framework.permissions import SAFE_METHODS
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsAdminOrIfAuthenticatedReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return bool(
-            (
-                request.method in SAFE_METHODS
-                and request.user
-                and request.user.is_authenticated
-            )
-                or (request.user and request.user.is_staff)
-        )
+class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+   def has_permission(self, request, view):
+       return bool(
+           (
+               request.method in SAFE_METHODS
+               and request.user
+               and request.user.is_authenticated
+           )
+               or (request.user and request.user.is_staff)
+       )
