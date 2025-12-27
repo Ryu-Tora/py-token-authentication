32a57ce5979f8b6e91efe2d8b2966b529b781f15
Adam Halka <galkaadam4@gmail.com>
2025-12-27 23:13:59 +0100

    Solution

--- user/permissions.py
+++ user/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
+from cinema.views import OrderViewSet
+
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
@@ -7,6 +9,12 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
             return False
 
         if request.method in SAFE_METHODS:
+            return True
+
+        if (
+                request.method == "POST"
+                and view.__class__ == OrderViewSet
+        ):
             return True
 
         return request.user.is_staff
