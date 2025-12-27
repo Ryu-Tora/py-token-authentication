368af6a9b8827a9757f484b1b45d5c94263716bc
Adam Halka <galkaadam4@gmail.com>
2025-12-27 22:38:06 +0100

    Solution

--- user/urls.py
+++ user/urls.py
@@ -7,5 +7,5 @@ app_name = "user"
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
     path("login/", CreateTokenView.as_view(), name="login"),
-    path("me/", ManageUserView.as_view(), name="manage"),
+    path("me/", ManageUserView.as_view(), name="me"),
 ]
